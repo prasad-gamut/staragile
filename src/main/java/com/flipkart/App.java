e2320f789c21b0da9c277855dc7595dab3af41e8
root <root@ip-172-31-41-86.ec2.internal>
2024-01-05 15:58:48 +0000

    adding files

---
+++ src/main/java/com/flipkart/App.java
@@ -0,0 +1,16 @@
+package com.flipkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
